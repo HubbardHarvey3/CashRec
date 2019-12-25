c213b5c1af9c323caed5b8c2e3078f2cfde60b9d
HubbardHarvey3 <hubbard.harvey@gmail.com>
2019-12-25 15:21:31 -0500

    Add project files.

---
+++ CashRec/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CashRec
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Initialize our donorDictionary
+        Dictionary<Int32, string> donorDictionary = new Dictionary<int, string>();
+        
+        //store our PATH into a string variable
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+@"\test.csv";
+        //Creating a donorList class with two objects Name and Number as opposed to donorDicitionary
+        public class donorList
+        {
+            public Int32 donorNum { get; set; }
+            public string donorName { get; set; }
+        }
+        //Initialize a new List using the donorList Class.
+        List<donorList> donors = new List<donorList>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Initializing the donorList Class and adding people hard code after program initializes
+            //Commented out troubleshooting and sending info to the console
+            //foreach (var item in donors)
+            //{
+            //    Console.WriteLine($"{item.donorNum}: {item.donorName}");
+            //}
+            //Reads CSV and adds the values to List
+            using (StreamReader sr = new StreamReader(path))
+            {
+                //create a string variable per line read from the CSV
+                string row;
+                
+                //While the row variable isn't null, split the row into an array to be added to the Key/Value Dictionary
+                while ((row = sr.ReadLine()) != null)
+                {
+                    String[] strlist = row.Split(",");
+                    //Check to ensure the first substring isn't empty
+                    if (strlist[0] != "")
+                    {
+                        //Add the substrings to the dictionary
+                        //Show create Try Catch in case the dictionary already contains the information being added.
+                        donors.Add(new donorList() { donorNum = Convert.ToInt32(strlist[0]), donorName = strlist[1] });
+                        Console.WriteLine($"{strlist[0]} : {strlist[1]} added to donors list.");
+                    }
+                }
+                //Once the CSV is added to the dictionary, write the dictionary to the Donor List output.
+                //foreach (KeyValuePair<Int32, string> entry in donorDictionary)
+                //{
+                //    DonorListDisplay.Text += string.Format("{0}, {1}" + Environment.NewLine, entry.Key.ToString(), entry.Value);
+                //};
+                foreach (var item in donors)
+                {
+                    DonorListDisplay.Text += $"{ item.donorNum }: { item.donorName }" + Environment.NewLine;
+                }
+
+            }
+
+            //Setting the source of the datagrid to our recently created donorList, donors
+            dgSimple.ItemsSource = donors;
+        }
+        //I don't know what these two below do:
+        private void DonorListDisplay_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void DailyRecordInput_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        //I don't know what these two above do:
+
+        //Uses DonorDictionary
+        private void SumbitNewDonor(object sender, RoutedEventArgs e)
+        {
+            //setup variables for the Input Textboxes
+            string name = DonorNameInput.Text;
+            string num = DonorNumInput.Text;
+            //Add the information in the textboxes to our list.
+            donors.Add(new donorList() { donorNum = (Convert.ToInt32(num)), donorName = name });
+            //Display pop up confirming users submission
+            MessageBox.Show($"Your new Donor {name} is saved!");
+            //reset the text boxes so duplicates can't be added.
+            DonorNameInput.Text = "";
+            DonorNumInput.Text = "";
+        }
+
+        //Display reads from the csv file and adds what it reads to the donorDictionary.  
+        //Display also writes the dictionary to the Donor List Output.
+        public void Display(object sender, RoutedEventArgs e)
+        {
+            //Reset the DonorList Display
+            DonorListDisplay.Text = "";
+            //Loop through the list and add each element to the textbox
+            foreach (var item in donors)
+            {
+                DonorListDisplay.Text += $"{item.donorNum}: {item.donorName}" + Environment.NewLine;
+            }
+
+        }
+        //Uses DonorDictionary
+        private void SaveDonorList(object sender, RoutedEventArgs e)
+        {
+            //Clean the CSV File of records
+            File.WriteAllText(path, "");
+            //loop through the donors list and write the records in the CSV file.
+            foreach (var item in donors)
+            {
+                String csv = String.Join(Environment.NewLine, $"{item.donorNum}, {item.donorName}"+Environment.NewLine);
+                File.AppendAllText(path, csv);
+            }
+        }
+
+
+    }
+
+}
