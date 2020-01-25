cb1c2b0da27302c5d9ba3981b1963fd549ab2e8f
HubbardHarvey3 <hubbard.harvey@gmail.com>
2020-01-25 17:14:38 -0500

    Deleted the old even handlers and imported Donor Class.

---
+++ CashRec/Donor.cs
@@ -0,0 +1,108 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CashRec
+{
+    public class Donor
+    {
+        //FilePath for JSON doc
+        public static string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\text.txt";
+
+        //Initialize the Dictionary
+        public static Dictionary<int, string> Dlist = new Dictionary<int, string>();
+
+        //Properties of the Donor Class
+        public int number { get; set; }
+        public string fullName { get; set; }
+
+        //Donor Constructor
+        public Donor(int _number, string _lastName, string _firstName)
+        {
+            this.number = _number;
+            this.fullName = $"{_lastName}, {_firstName}";
+        }
+        //Write the Donor List to the console and overwrite the JSON file on the desktop
+
+        public static Dictionary<int, string> temp = new Dictionary<int, string>();
+        public static void writeDlist()
+        {
+
+            //print dictionary to console
+
+            //reorder the dictionary before printing it
+            //In this foreach, sort the existing Dlist and add to temp dictionary
+            foreach (KeyValuePair<int, string> donor in Dlist.OrderBy(key => key.Key))
+            {
+                temp.Add(donor.Key, donor.Value);
+            }
+            //with the data added to temp, clear everything in Dlist
+            Dlist.Clear();
+            //with Dlist clean, add back the sorted dictionary from the temp dictionary
+            foreach (KeyValuePair<int, string> item in temp)
+            {
+                Dlist.Add(item.Key, item.Value);
+            }
+            //we are done with Temp, clear it out for use next time
+            temp.Clear();
+
+            Console.WriteLine("The below will be added to the JSON file");
+            Console.WriteLine("******************************************");
+            foreach (var item in Donor.Dlist)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+            Console.WriteLine("******************************************");
+            //serialize the dictionary
+            string jsonA = JsonConvert.SerializeObject(Donor.Dlist, Formatting.Indented);
+
+            //create JSON file
+            File.WriteAllText(Donor.path, jsonA);
+        }
+
+        //Read an existing JSON file and add to the dictionary
+        public static void readDlist()
+        {
+            //Read all text in the Text.txt file and deserialize it.  
+            var json = JsonConvert.DeserializeObject(File.ReadAllText(path));
+            //take the deserialized data and add it to the Dlist Dictionary
+            Dictionary<int, string> Temp = JsonConvert.DeserializeObject<Dictionary<int, string>>(json.ToString());
+            Console.WriteLine("The below was read from the JSON File");
+            Console.WriteLine("******************************************");
+            //Dictionary<int, string> json = JsonConvert.DeserializeObject<Dictionary<int, string>>(File.ReadAllText(path));
+            foreach (var item in Temp)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+                Donor.Dlist.Add(item.Key, item.Value);
+
+            }
+            Console.WriteLine("******************************************");
+
+        }
+
+        public static void addDlist(string Dnum, string firstName, string lastName)
+        {
+            //add donor to dictionary
+            Dlist.Add(Convert.ToInt32(Dnum), $"{lastName}, {firstName}");
+            writeDlist();
+        }
+
+        public static void removeDlist()
+        {
+            Console.WriteLine("Do you want to remove someone? Y/N");
+            string answer = Console.ReadLine();
+            answer = answer.ToLower();
+
+            if (answer == "y")
+            {
+                Console.WriteLine("Please enter their donor number:");
+                string num = Console.ReadLine();
+                Dlist.Remove(Convert.ToInt32(num));
+                writeDlist();
+            }
+        }
+
+    }
+}
--- CashRec/MainWindow.xaml.cs
+++ CashRec/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace CashRec
 {
@@ -11,56 +12,12 @@ namespace CashRec
     /// </summary>
     public partial class MainWindow : Window
     {
-        //Initialize our donorDictionary
-        Dictionary<Int32, string> donorDictionary = new Dictionary<int, string>();
         
-        //store our PATH into a string variable
-        string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+@"\test.csv";
-        //Creating a donorList class with two objects Name and Number as opposed to donorDicitionary
-        public class donorList
-        {
-            public Int32 donorNum { get; set; }
-            public string donorName { get; set; }
-        }
-        //Initialize a new List using the donorList Class.
-        List<donorList> donors = new List<donorList>();
         public MainWindow()
         {
             InitializeComponent();
-            //Initializing the donorList Class and adding people hard code after program initializes
-            
-            //Reads CSV and adds the values to List
-            using (StreamReader sr = new StreamReader(path))
-            {
-                //create a string variable per line read from the CSV
-                string row;
-                
-                //While the row variable isn't null, split the row into an array to be added to the Key/Value Dictionary
-                while ((row = sr.ReadLine()) != null)
-                {
-                    String[] strlist = row.Split(",");
-                    //Check to ensure the first substring isn't empty
-                    if (strlist[0] != "")
-                    {
-                        //Add the substrings to the dictionary
-                        //Show create Try Catch in case the dictionary already contains the information being added.
-                        donors.Add(new donorList() { donorNum = Convert.ToInt32(strlist[0]), donorName = strlist[1] });
-                        Console.WriteLine($"{strlist[0]} : {strlist[1]} added to donors list.");
-                    }
-                }
-                //Add the List to the DonorList Display Text Box for the User
-                foreach (var item in donors)
-                {
-                    DonorListDisplay.Text += $"{ item.donorNum }: { item.donorName }" + Environment.NewLine;
-                    DonorComboBox.Items.Add(item.donorNum.ToString() + " " + item.donorName);
-                }
-
-            }
-
-            //Setting the source of the datagrid to our recently created donorList, donors
-            DailyRecGrid.ItemsSource = donors;
-            
-            
+            //Reads the JSON file and adds the entries to the DList Dictionary
+            Donor.readDlist();
         }
         //I don't know what these two below do:
         private void DonorListDisplay_TextChanged(object sender, TextChangedEventArgs e)
@@ -71,53 +28,28 @@ namespace CashRec
         {
 
         }
-        //I don't know what these two above do:
 
-        //Uses donors list
-        private void SumbitNewDonor(object sender, RoutedEventArgs e)
+        private void writeDlist(object sender, RoutedEventArgs e)
         {
-            //setup variables for the Input Textboxes
-            string name = DonorNameInput.Text;
-            string num = DonorNumInput.Text;
-            //Add the information in the textboxes to our list.
-            donors.Add(new donorList() { donorNum = (Convert.ToInt32(num)), donorName = name });
-            //Display pop up confirming users submission
-            MessageBox.Show($"Your new Donor {name} is saved!");
-            //reset the text boxes so duplicates can't be added.
-            DonorNameInput.Text = "";
+            Donor.writeDlist();
+        }
+
+        //Submit Button Event Handler.  Grabs the various inputs and writes them to the JSON File
+        private void SubmitDonor(object sender, RoutedEventArgs e)
+        {
+            string Dnum = DonorNumInput.Text;
+            string FirstName = DonorFirstNameInput.Text;
+            string LastName = DonorLastNameInput.Text;
+            Donor.addDlist(Dnum, FirstName, LastName);
+            Donor.writeDlist();
+
             DonorNumInput.Text = "";
+            DonorFirstNameInput.Text = "";
+            DonorLastNameInput.Text = "";
         }
+        
 
-        //Display reads from the csv file and adds what it reads to the donorDictionary.  
-        //Display also writes the dictionary to the Donor List Output.
-        public void Display(object sender, RoutedEventArgs e)
-        {
-            //Reset the DonorList Display
-            DonorListDisplay.Text = "";
-            //Loop through the list and add each element to the textbox
-            foreach (var item in donors)
-            {
-                DonorListDisplay.Text += $"{item.donorNum}: {item.donorName}" + Environment.NewLine;
-            }
 
-        }
-        //Uses donors list
-        private void SaveDonorList(object sender, RoutedEventArgs e)
-        {
-            //Clean the CSV File of records
-            File.WriteAllText(path, "");
-            //loop through the donors list and write the records in the CSV file.
-            foreach (var item in donors)
-            {
-                String csv = String.Join(Environment.NewLine, $"{item.donorNum}, {item.donorName}"+Environment.NewLine);
-                File.AppendAllText(path, csv);
-            }
-        }
-
-        private void DonorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
     }
 
 }
