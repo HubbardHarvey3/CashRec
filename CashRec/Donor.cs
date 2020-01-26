2fac492901f42e1c1c9b74b505e6b48fe2d211de
HubbardHarvey3 <hubbard.harvey@gmail.com>
2020-01-26 15:20:05 -0500

    Add Try Catch exception block to ensure the user can't enter the same donor number twice

--- CashRec/Donor.cs
+++ CashRec/Donor.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Windows;
 
 namespace CashRec
 {
@@ -12,7 +13,7 @@ namespace CashRec
         public static string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\text.txt";
 
         //Initialize the Dictionary
-        public static Dictionary<int, string> Dlist = new Dictionary<int, string>();
+
 
         //Properties of the Donor Class
         public int number { get; set; }
@@ -27,36 +28,30 @@ namespace CashRec
         //Write the Donor List to the console and overwrite the JSON file on the desktop
 
         public static Dictionary<int, string> temp = new Dictionary<int, string>();
-        public static void writeDlist()
+        public static void savetoDlist()
         {
 
             //print dictionary to console
 
             //reorder the dictionary before printing it
             //In this foreach, sort the existing Dlist and add to temp dictionary
-            foreach (KeyValuePair<int, string> donor in Dlist.OrderBy(key => key.Key))
+            foreach (KeyValuePair<int, string> donor in MainWindow.Dlist.OrderBy(key => key.Key))
             {
                 temp.Add(donor.Key, donor.Value);
             }
             //with the data added to temp, clear everything in Dlist
-            Dlist.Clear();
+            MainWindow.Dlist.Clear();
             //with Dlist clean, add back the sorted dictionary from the temp dictionary
             foreach (KeyValuePair<int, string> item in temp)
             {
-                Dlist.Add(item.Key, item.Value);
+                MainWindow.Dlist.Add(item.Key, item.Value);
             }
             //we are done with Temp, clear it out for use next time
             temp.Clear();
 
-            Console.WriteLine("The below will be added to the JSON file");
-            Console.WriteLine("******************************************");
-            foreach (var item in Donor.Dlist)
-            {
-                Console.WriteLine($"{item.Key}: {item.Value}");
-            }
-            Console.WriteLine("******************************************");
+
             //serialize the dictionary
-            string jsonA = JsonConvert.SerializeObject(Donor.Dlist, Formatting.Indented);
+            string jsonA = JsonConvert.SerializeObject(MainWindow.Dlist, Formatting.Indented);
 
             //create JSON file
             File.WriteAllText(Donor.path, jsonA);
@@ -69,39 +64,33 @@ namespace CashRec
             var json = JsonConvert.DeserializeObject(File.ReadAllText(path));
             //take the deserialized data and add it to the Dlist Dictionary
             Dictionary<int, string> Temp = JsonConvert.DeserializeObject<Dictionary<int, string>>(json.ToString());
-            Console.WriteLine("The below was read from the JSON File");
-            Console.WriteLine("******************************************");
-            //Dictionary<int, string> json = JsonConvert.DeserializeObject<Dictionary<int, string>>(File.ReadAllText(path));
+            //Add the deserialized data to our Dlist dictionary
             foreach (var item in Temp)
             {
-                Console.WriteLine(item.Key + " " + item.Value);
-                Donor.Dlist.Add(item.Key, item.Value);
-
+                MainWindow.Dlist.Add(item.Key, item.Value);
             }
-            Console.WriteLine("******************************************");
-
         }
 
         public static void addDlist(string Dnum, string firstName, string lastName)
         {
             //add donor to dictionary
-            Dlist.Add(Convert.ToInt32(Dnum), $"{lastName}, {firstName}");
-            writeDlist();
+            //Learning Try Catch Exemptions
+            try
+            {
+                MainWindow.Dlist.Add(Convert.ToInt32(Dnum), $"{lastName}, {firstName}");
+                savetoDlist();
+            }
+            catch (ArgumentException e)
+            {
+                MessageBox.Show("Donor # already in use");
+            }
+
         }
 
-        public static void removeDlist()
+        public static void removeDlist(string num)
         {
-            Console.WriteLine("Do you want to remove someone? Y/N");
-            string answer = Console.ReadLine();
-            answer = answer.ToLower();
-
-            if (answer == "y")
-            {
-                Console.WriteLine("Please enter their donor number:");
-                string num = Console.ReadLine();
-                Dlist.Remove(Convert.ToInt32(num));
-                writeDlist();
-            }
+            MainWindow.Dlist.Remove(Convert.ToInt32(num));
+            savetoDlist();
         }
 
     }
--- CashRec/MainWindow.xaml.cs
+++ CashRec/MainWindow.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 
 namespace CashRec
 {
@@ -12,12 +10,17 @@ namespace CashRec
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        public static Dictionary<int, string> Dlist = new Dictionary<int, string>();
+
         public MainWindow()
         {
+
             InitializeComponent();
             //Reads the JSON file and adds the entries to the DList Dictionary
             Donor.readDlist();
+            //Writes the dictionary to the textbox
+            writeDlist();
+
         }
         //I don't know what these two below do:
         private void DonorListDisplay_TextChanged(object sender, TextChangedEventArgs e)
@@ -29,9 +32,19 @@ namespace CashRec
 
         }
 
-        private void writeDlist(object sender, RoutedEventArgs e)
+
+        //*************EVENT HANDLERS BELOW
+        public void writeDlist([Optional] object sender, [Optional] RoutedEventArgs e)
         {
-            Donor.writeDlist();
+            Donor.savetoDlist();
+            //Add each dictionary pair to an empty string and then display that string to the output box.
+            string line = "";
+            foreach (var item in Dlist)
+            {
+                line += $"{item.Key}: {item.Value} \n";
+
+            }
+            DonorListOutput.Text = line;
         }
 
         //Submit Button Event Handler.  Grabs the various inputs and writes them to the JSON File
@@ -41,15 +54,23 @@ namespace CashRec
             string FirstName = DonorFirstNameInput.Text;
             string LastName = DonorLastNameInput.Text;
             Donor.addDlist(Dnum, FirstName, LastName);
-            Donor.writeDlist();
+            Donor.savetoDlist();
 
             DonorNumInput.Text = "";
             DonorFirstNameInput.Text = "";
             DonorLastNameInput.Text = "";
+            writeDlist();
         }
-        
 
+        private void DeleteDonorNum(object sender, RoutedEventArgs e)
+        {
+            string Donornum = DonorNumInput.Text;
+            Donor.removeDlist(Donornum);
+            writeDlist();
+            DonorNumInput.Text = "";
+        }
 
+        //*************EVENT HANDLERS ABOVE
     }
 
 }
