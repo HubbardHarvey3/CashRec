c1198bf97bc216be8d2a27d1419fb7a5127e4fed
HubbardHarvey3 <hubbard.harvey@gmail.com>
2020-01-30 21:59:38 -0500

    BIG UPDATE: Include donor list to Transaction Tab for reference, allow user to delete transactions, total transactions for user

--- CashRec/MainWindow.xaml.cs
+++ CashRec/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
@@ -8,10 +10,15 @@ namespace CashRec
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
+        //Dictionary that holds everything from the Donor Class
         public static Dictionary<int, string> Dlist = new Dictionary<int, string>();
-
+        //List that takes entries from Dlist and helps to spit them into CSV File
+        public List<Transaction> TransactList = new List<Transaction>();
+        //Filepath for CSV File which will record entries made on the transaction grid
+        string csvFile = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\TestCSV.csv";
         public MainWindow()
         {
 
@@ -33,7 +40,7 @@ namespace CashRec
         }
 
 
-        //*************EVENT HANDLERS BELOW
+        //*************EVENT HANDLERS BELOW for Donor List Tab
         public void writeDlist([Optional] object sender, [Optional] RoutedEventArgs e)
         {
             Donor.savetoDlist();
@@ -45,6 +52,7 @@ namespace CashRec
 
             }
             DonorListOutput.Text = line;
+            DonorListOutputA.Text = line;
         }
 
         //Submit Button Event Handler.  Grabs the various inputs and writes them to the JSON File
@@ -70,7 +78,75 @@ namespace CashRec
             DonorNumInput.Text = "";
         }
 
-        //*************EVENT HANDLERS ABOVE
+
+
+        //*************EVENT HANDLERS ABOVE for Donor List Tab
+
+
+        //Event Handlers for Transaction Class
+        private void ListTransactions(object sender, RoutedEventArgs e)
+        {
+            //set the Itemsource to the new list TransactList
+            TransactionDataGrid.ItemsSource = TransactList;
+            //Use a foreach to add Dlist dicitionary to Transactlist on the Transaction Class
+            foreach (var item in Dlist)
+            {
+                //only adds if the Dlist.Key is valid
+                if (item.Key == Convert.ToInt32(DonorNumTransactionInput.Text))
+                {
+                    TransactList.Add(new Transaction { date = TransactionDateInput.Text, donorNum = item.Key, name = item.Value, amount = Convert.ToDouble(TransactionAmountInput.Text) });
+                    //must refresh the DataGrid to include each row.
+                    TransactionDataGrid.Items.Refresh();
+                }
+
+            }
+
+        }
+
+        private void ExportList(object sender, RoutedEventArgs e)
+        {
+            //start by adding the column headers to the string
+            string csv = "DATE,Donor Number,Last Name, First Name, Donated Amount";
+            //add each item from the TransactList to the string, ensuring to start a new line after before each entry
+            foreach (var item in TransactList)
+            {
+                csv += "\r\n";
+                csv += $"{item.date},{item.donorNum},{item.name},${item.amount}";
+            }
+            //Found this on Microsofts website https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-write-to-a-text-file
+            //Uses Streamwriter to APPEND the string in CSV to the existing file.
+            using (StreamWriter file =
+                new StreamWriter(csvFile, true))
+            {
+                file.WriteLine("\r\n" + csv);
+            }
+        }
+
+        private void DeleteRow(object sender, RoutedEventArgs e)
+        {
+
+            foreach (var item in TransactList)
+            {
+                if (item.donorNum == Convert.ToInt32(DonorNumTransactionInput.Text) && item.amount == Convert.ToDouble(TransactionAmountInput.Text))
+                {
+                    TransactList.Remove(item);
+                    break;
+                }
+            }
+            TransactionDataGrid.Items.Refresh();
+        }
+
+        private void total(object sender, RoutedEventArgs e)
+        {
+            double total = 0;
+            foreach (var item in TransactList)
+            {
+                total += item.amount;
+            }
+            MessageBox.Show("The Total is:$" + total);
+        }
     }
 
+    //Stop user from entering unused DonorNumber
+    //Print Donor List functionality
 }
---
+++ CashRec/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Runtime.InteropServices;
+using System.Windows.Controls;
+using System.Globalization;
+
+namespace CashRec
+{
+    
+    public class Transaction
+    {
+        
+        public int donorNum { get; set; }
+        public string name { get; set; }
+        public string date { get; set; }
+        public double amount { get; set; }
+
+
+    }
+}
